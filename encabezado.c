5359092872661e699b5554c98fe13f56db5cae3b
waltercuello11 <walter.cuello@upb.edu.co>
2025-03-27 09:04:28 -0500

    encabezado

--- encabezado.c
+++ encabezado.c
@@ -1,2 +1,16 @@
-
-
+#include <studio.h>
+void encabezado (void);
+int main( int arg,char const*argv[])
+{
+    encabezado();
+    return 0;
+}
+    void encabezado(void)
+{
+    printf(**********************);
+    printf("*iniversidad pontificia bolivariana*");
+    printf("*escuela ingeriera*");
+    printf("*walter miguel cuello*");
+    printf("*000563762");
+    printf("********************");
+}
