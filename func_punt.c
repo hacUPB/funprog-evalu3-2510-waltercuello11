5da5c5f9e94da7fe4e8ec38d66576f0d10664055
waltercuello11 <walter.cuello@upb.edu.co>
2025-04-01 09:26:29 -0500

    punteros

---
+++ func_punt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+// Función que recibe dos punteros y realiza operaciones con ellos
+void operacion_punteros(float *, double *);
+
+int main(int argc, char const *argv[])
+{
+    float cubo;
+    double coseno ;
+
+    operacion_punteros(&cubo, &coseno);
+    
+    printf("Cubo = %f\n", cubo);
+    printf("coseno = %d\n", coseno);
+
+    return 0;
+}
+
+
+// Definición de la función
+void operacion_punteros(float *var1, double *var2)
+{
+    // En la variable a la que apunta var1 voy a guardar el cubo de un número
+    // que ingresa el usuario y en la variable a la que apunta var2 el factorial
+
+    float dato;
+    printf("Ingresa un dato: \n");
+    scanf("%d", &dato);
+
+    (*var1) = pow(dato, 3);  // Guarda en la variable cubo = dato^3
+    (*var2) = cos(dato);     // Guarda en la variable factorial = fact(dato)
+}
